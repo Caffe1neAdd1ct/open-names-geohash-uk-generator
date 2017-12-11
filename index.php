6ba491050cf9f0d5286051c1bd5b33dea303f570
Kevin Andrews <kevin@zvps.uk>
2017-12-11 21:15:05 +0000

    IDE type hints added for custom defined constants.

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 #!/usr/bin/env php
 <?php
 
+/** @var string Constant pointing to the application parent directory */
 define('APP_DIR', __DIR__);
+/** @var string Short replacement for the DIRECTORY_SEPARATOR constant */
 define('DS', DIRECTORY_SEPARATOR);
 
 require_once __DIR__ . '/vendor/autoload.php';
