d90e841ed7fc8f1b9d4fd2d9ad1e866effeb0da1
Kevin Andrews <kevin@zvps.uk>
2017-12-11 12:00:47 +0000

    Added shebang line for executing index.php directly and checked pdo connection.

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+#!/usr/bin/env php
 <?php
 
 use Symfony\Component\Console\Output\OutputInterface;
@@ -9,27 +10,19 @@ $app = new Silly\Edition\PhpDi\Application();
 
 require_once __DIR__ . '/app/bootstrap.php';
 
-
-
-
-
-$app->command('run', function (OutputInterface $output) use ($app) {
+/**
+ * Run Command
+ * php index.php run
+ */
+$app->command('run', function (OutputInterface $output, Psr\Log\LoggerInterface $logger, ProgressBar $progress) use ($app) {
     
-    var_dump($app->getContainer()->get('config'));
     
+    /* @var $dbConnection Slim\PDO\Database */
+    $dbConnection = $app->getContainer()->get(Slim\PDO\Database::class);
+
     $output->writeln('Starting run command.');
     // create a new progress bar (50 units)
-    $progress = new ProgressBar($output, 50);
-$progress->setFormat(sprintf('%s item: <info>%%item%%</info>', 
-$progress->getFormatDefinition('debug'))); // the new format
-$progress->setBarCharacter('<fg=green>⚬</>');
-$progress->setEmptyBarCharacter("<fg=red>⚬</>");
-$progress->setProgressCharacter("<fg=green>➤</>");
-$progress->setFormat(
-    "<fg=white;bg=cyan> %status:-45s%</>\n%current%/%max% [%bar%] %percent:3s%%\n  %estimated:-20s%  %memory:20s%"
-);
-    // start and displays the progress bar
-    $progress->setMessage("Starting...", 'status');
+    
     $progress->start();
 
     $i = 0;
