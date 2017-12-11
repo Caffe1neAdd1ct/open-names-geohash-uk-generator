d197ecf94b358a83bf76ae40b7114aed831e609e
Kevin Andrews <kevin@zvps.uk>
2017-12-11 11:28:59 +0000

    Bootstrap adds config, pdo connection and progress bar instance to app container.

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,30 +1,63 @@
 <?php
 
+use Symfony\Component\Yaml\Yaml;
+use Symfony\Component\Console\Helper\ProgressBar;
+use Slim\PDO\Database;
+
 /* @var $app Silly\Application */
+/* @var $container DI\Container */
+$container = $app->getContainer();
 
 /**
  * Config Loader
  */
-$app->getContainer()->set('config', function() {
-    return [
-        'db_host' => 'localhost',
-        'db_driver' => 'mysql',
-        'db_user' => 'root',
-        'db_pass' => 'letmein',
-        'data_dir' => realpath(__DIR__ . '/../data')
-        
-    ];
+$container->set('config', function($container) {
+    return Yaml::parseFile(__DIR__ . '/config/config.yaml');
 });
 
 /**
  * DB Connection
  */
+$container->set(Database::class, function() use ($container) {
+    
+    $dbConfig = $container->get('config')['db'];
+    
+    $dns = $dbConfig['driver'] . 
+        ':host=' . $dbConfig['host'] . 
+        ';dbname=' . $dbConfig['schema'] . 
+        ';charset=' . $dbConfig['charset'] . 
+        ';collation=' . $dbConfig['collation'];
+    
+    return new Database($dns, $dbConfig['user'], $dbConfig['pass'], (array) $dbConfig['options']);
+});
 
 /**
- * CSV Processor
+ * Cli Progress Bar
+ */
+$container->set(ProgressBar::class, function() use ($container)
+{
+    $progress = new ProgressBar($container->get(Symfony\Component\Console\Output\ConsoleOutput::class), 1);
+    $progress->setFormat(sprintf('%s item: <info>%%item%%</info>', $progress->getFormatDefinition('debug')));
+    $progress->setBarCharacter('<fg=green>⚬</>');
+    $progress->setEmptyBarCharacter("<fg=red>⚬</>");
+    $progress->setProgressCharacter("<fg=green>➤</>");
+    $progress->setFormat("<fg=white;bg=cyan> %status:-45s%</>\n%current%/%max% [%bar%] %percent:3s%%\n  %estimated:-20s%  %memory:20s%");
+    $progress->setMessage("Starting up the cogs and wheels", 'status');
+    
+    return $progress;
+});
+
+
+/**
+ * CSV Reader
+ */
+
+
+/**
+ * CSV Writer
  */
 
 /**
- * PHP Loc Lib
+ * PHP Coor Lib
  */
 
