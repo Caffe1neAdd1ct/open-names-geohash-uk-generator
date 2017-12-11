dce5a74649a5a3393961dbb2a0a6088d7bca740b
Kevin Andrews <kevin@zvps.uk>
2017-12-11 15:46:07 +0000

    Helper constants added and support for unlimited args on commands.

--- index.php
+++ index.php
@@ -1,19 +1,20 @@
 #!/usr/bin/env php
 <?php
 
+define('APP_DIR', __DIR__);
+define('DS', DIRECTORY_SEPARATOR);
+
 require_once __DIR__ . '/vendor/autoload.php';
-
 $app = new Silly\Edition\PhpDi\Application();
-
 require_once __DIR__ . '/app/bootstrap.php';
 
 /**
  * Run Command
  * php index.php run
  */
-$app->command('run', 'App\Command\Run');
-$app->command('extract [file]', 'App\Command\Extract');
-$app->command('process', 'App\Command\Process');
-$app->command('export', 'App\Command\Export');
+$app->command('run [args]*', 'App\Command\Run');
+$app->command('extract [args]*', 'App\Command\Extract');
+$app->command('process [args]*', 'App\Command\Process');
+$app->command('export [args]*', 'App\Command\Export');
 
 $app->run();
